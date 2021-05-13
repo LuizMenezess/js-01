26060770d3f22a1b696c7cf39468c9cb9f3024ad
LuizMenezess <luizmenezes334@gmail.com>
2021-05-13 20:57:16 -0300

    adicionando desafio js-01

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = "Luiz"
+let idade = 21
+let ano = 2021
+
+console.log (`Meu ano de nascimento é ${ano - idade}`)  
+
+let souhumano = true
+
+let number = 12
+let numero = 23
+
+console.log (`${number + numero}`)  
